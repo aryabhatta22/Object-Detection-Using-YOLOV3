a72d6362d56ceb7c36400ad82ce7c7b8ad07e9ae
aryabhatta <tarun.hive@hotmail.com>
2020-08-09 21:31:29 +0530

    added: bounding box function

--- yolo_utils.py
+++ yolo_utils.py
@@ -13,3 +13,31 @@ def draw_labels_and_boxes(img, boxes, confidences, classIds, idxs, colors, label
             text = "{}: {:4f}".format(labels[classIds[i]], confidences[i])
             cv.putText(img, text, (x, y-5),  cv.FONT_HERSHEY_SIMPLEX, 0.5, COLOR, 2)
     return img
+
+def generate_boxes_confidences_classids(outs, height, width,tconf):
+    """
+    tconf is threshold confidence
+    """
+
+    boxes = []
+    confidences = []
+    classids = []
+
+    for out in outs:
+        for detection in outs:
+        scores = detections[5:]
+        classid = np.argmax(scores)
+        confidence = scores[classid]
+
+        if confidence > tconf:
+            box = detection[0:4] * np.array([width, height, width, height])
+            centerX, centerY, bwidth, bheight = box.astype('int')
+
+            // top coordinates
+
+            x = int(centerX - (bwidth/2))
+            y = int(centerY - (bheight/2))
+            boxes.append([x,y, int(bwidth), int(bheight)])
+            confidences.append(float(confidence))
+            classids.append(classid)
+    return boxes, confidences, classids
